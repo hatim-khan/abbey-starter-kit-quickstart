0d5cc99c2a2efc737a0bddf64a00e37318168625
Hatim Khan <bluefireh@gmail.com>
2023-08-18 17:01:07 -0700

    rename kit

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "http" {
-    address        = "https://api.abbey.io/terraform-http-backend"
-    lock_address   = "https://api.abbey.io/terraform-http-backend/lock"
-    unlock_address = "https://api.abbey.io/terraform-http-backend/unlock"
+    address        = "http://localhost:8080/terraform-http-backend"
+    lock_address   = "http://localhost:8080/terraform-http-backend/lock"
+    unlock_address = "http://localhost:8080/terraform-http-backend/unlock"
     lock_method    = "POST"
     unlock_method  = "POST"
   }
@@ -18,10 +18,11 @@ terraform {
 provider "abbey" {
   # Configuration options
   bearer_auth = var.abbey_token
+  server_url = "http://localhost:8080/v1/"
 }
 
-resource "abbey_grant_kit" "abbey_demo_site" {
-  name = "Abbey_Demo_Site"
+resource "abbey_grant_kit" "abbey_demo_site_kit" {
+  name = "Abbey_Demo_Site_Kit"
   description = <<-EOT
     Grants access to Abbey's Demo Page.
   EOT
