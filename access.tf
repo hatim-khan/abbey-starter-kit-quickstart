562a3ebe5b6f101eab1ab4ac1dcce5920d0c687d
Abbey <no-reply@abbey.io>
2023-08-18 17:11:39 -0700

    hat+test@abbey.io requests access to: Abbey_Demo_Site_Kit

--- access.tf
+++ access.tf
@@ -2,3 +2,7 @@ resource "abbey_demo" "grant_read_write_access" {
   permission = "read_write"
   email = "hat+test@abbey.io"
 }
+resource "abbey_demo" "grant_read_write_access" {
+  permission = "read_write"
+  email = "hat+test@abbey.io"
+}
