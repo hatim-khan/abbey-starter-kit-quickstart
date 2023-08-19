80deab962c233b9d904d54d89af77ca3530a885b
Hatim Khan <bluefireh@gmail.com>
2023-08-18 17:41:53 -0700

    remove permissions so revoke workflow silently fails

--- access.tf
+++ access.tf
@@ -1,8 +1,1 @@
-resource "abbey_demo" "grant_read_write_access" {
-  permission = "read_write"
-  email = "hat+test@abbey.io"
-}
-resource "abbey_demo" "grant_read_write_access" {
-  permission = "read_write"
-  email = "hat+test@abbey.io"
-}
+
