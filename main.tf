b6f4840e52fb09d08964760206f194e9f6341e27
Hatim Khan <bluefireh@gmail.com>
2023-11-16 14:01:09 -0800

    test invalid policy

--- main.tf
+++ main.tf
@@ -20,53 +20,8 @@ provider "abbey" {
   bearer_auth = var.abbey_token
 }
 
-resource "abbey_grant_kit" "PagerDuty_Demo" {
-  name = "PagerDuty_Demo"
-  description = "PagerDuty demo"
-
-  workflow = {
-    steps = [
-      {
-        reviewers = {
-          one_of = ["hat@abbey.io"] # CHANGEME
-        },
-        skip_if = [
-          { bundle = "github://hatim-khan/abbey-starter-kit-quickstart/policies/on-call" } # CHANGEME
-        ]
-      }
-    ]
-  }
-
-  policies = [
-    {
-      query = <<REGO
-      package common
-
-      import data.abbey.functions
-
-      allow[msg] {
-          functions.expire_after("5m")
-          msg := sprintf("granting access for %s", ["5m"])
-      }
-      REGO
-    }
-  ]
-
-  output = {
-    # Replace with your own path pointing to where you want your access changes to manifest.
-    # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://hatim-khan/abbey-starter-kit-quickstart/access.tf" # CHANGEME
-    append = <<-EOT
-      resource "abbey_demo" "grant_read_write_access" { # {{ .data.system.abbey.identities.abbey.email | printf "%stest" }}
-        permission = "read_write"
-        email = "{{ .data.system.abbey.identities.abbey.email }}"
-      }
-    EOT
-  }
-}
-
-resource "abbey_grant_kit" "abbey_g_pumpkin" {
-  name = "Abbey_Pumpkin_key"
+resource "abbey_grant_kit" "abbey_invalid_policy" {
+  name = "abbey_invalid_policy"
   description = <<-EOT
     Grants access to Abbey's Demo Page.
   EOT
@@ -82,7 +37,7 @@ resource "abbey_grant_kit" "abbey_g_pumpkin" {
   }
 
   policies = [
-    { bundle = "github://hatim-khan/abbey-starter-kit-quickstart/policies" } # CHANGEME
+    { bundle = "github://hatim-khan/abbey-starter-kit-quickstart/missing_dir/policies" } # CHANGEME
   ]
 
   output = {
