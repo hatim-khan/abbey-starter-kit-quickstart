e6fec71db18d4e11c48752ab54679486e54661cf
Abbey <no-reply@abbey.io>
2023-12-13 00:54:07 +0000

    Revoking access to abbey_demo_site_one from hat@abbey.io

--- access.tf
+++ access.tf
@@ -1,12 +1,8 @@
-resource "abbey_demo" "grant_read_write_access_one" {
-  permission = "read_write"
-  email = "hat@abbey.io"
-}
 resource "abbey_demo" "grant_read_write_access_two" {
   permission = "read_write"
-  email = "hat@abbey.io"
+  email      = "hat@abbey.io"
 }
 resource "abbey_demo" "grant_read_write_access_three" {
   permission = "read_write"
-  email = "hat@abbey.io"
+  email      = "hat@abbey.io"
 }
