b9ce4b724dda1a61e43dda845c85adc09c6d2f6e
Abbey <no-reply@abbey.io>
2024-01-05 23:38:45 +0000

    [Abbey] hat@abbey.io requests access to: abbey_demo_site_two

--- access.tf
+++ access.tf
@@ -2,3 +2,7 @@ resource "abbey_demo" "grant_read_write_access" {
   permission = "read_write"
   email = "hat@abbey.io"
 }
+resource "abbey_demo" "grant_read_write_access_two" {
+  permission = "read_write"
+  email = "hat@abbey.io"
+}
